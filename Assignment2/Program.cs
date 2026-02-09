be8608485fcbee2f3afb4b86e5ac2740cb84936e
Abhishek Marotkar <abhishekmarotkar61@gmail.com>
2026-02-09 22:23:37 +0530

    Add Assignment2 - C# Fundamentals

---
+++ Assignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sections;
+
+class Program
+{
+    static void Main()
+    {
+        Section01_Intro.Run();
+        Section02_ProgramStructure.Run();
+        Section03_PrimitiveTypes.Run();
+        Section04_Strings.Run();
+        Section05_Var.Run();
+        Section06_Nullables.Run();
+        Section07_Conditionals.Run();
+        Section08_PatternMatching.Run();
+        Section09_LogicalOperators.Run();
+        Section10_Math.Run();
+        Section11_Collections.Run();
+        Section12_Loops.Run();
+        Section13_Comments.Run();
+        Section14_Namespaces.Run();
+        Section15_Debugging.Run();
+    }
+}
+
