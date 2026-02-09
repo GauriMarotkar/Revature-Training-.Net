866c083ce7d59604fb6ad8da425e5dda8f925a69
GauriMarotkar <gaurimarotkar@gmail.com>
2026-02-09 22:49:25 +0530

    Added README and updated under GauriMarotkar

--- Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
+++ Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8608485fcbee2f3afb4b86e5ac2740cb84936e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
