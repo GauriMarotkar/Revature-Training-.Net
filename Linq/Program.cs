16bbc1c0c9c445f4f0d5cd8f65a95692a0235f21
GauriMarotkar <gaurimarotkar@gmail.com>
2026-02-11 23:20:56 +0530

    Day 6- Delegates

---
+++ Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinqDemo linqDemo = new LinqDemo();
+        linqDemo.Run();
+
+        Console.ReadLine();
+    }
+}
